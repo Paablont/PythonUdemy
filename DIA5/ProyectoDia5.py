16e25c522371248f44acb2e7d0980a79f7bc394e
Pablo Villaseñor Ruiz <paablont@gmail.com>
2024-11-27 21:18:07 +0100

    Dia 5 proyecto parte 1

---
+++ DIA5/ProyectoDia5.py
@@ -0,0 +1,46 @@
+import random
+from random import choice
+
+palabras = ['pablito','arbol','cama','sol']
+
+palabra_oculta = random.choice(palabras)
+
+print(f'MODO DEV: {palabra_oculta}')
+
+#Metodo para mostrar los guiones según letras tenga la palabra
+def mostrar_guiones(palabra):
+    guiones = ''
+    for i in palabra:
+        print('_',end=' ')
+    print()
+
+#Metodo para comprobar si la letra esta en la palabra
+def comprobar_letra(letra,palabra_oculta):
+    if letra not in palabra_oculta:
+        print('La letra no está en la palabra')
+        return False
+    else:
+        pass
+    return True
+
+
+
+print(f'Bienvenido al juego del ahorcado. La palabra oculta es: ')
+mostrar_guiones(palabra_oculta)
+print()
+vidas = 5
+letra = ''
+while vidas > 0:
+
+        letra_usuario = input('Introduce una letra ').lower()
+        print()
+        if not comprobar_letra(letra_usuario,palabra_oculta):
+            vidas -= 1
+            print(f'Te quedan {vidas} vidas')
+        else:
+            for i in palabra_oculta:
+                if i == letra_usuario:
+                    print(i, end=' ')
+                else:
+                    print('_', end=' ')
+            print()
